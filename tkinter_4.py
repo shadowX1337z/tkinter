fe025661177c224e388f9da8179a43a38326baa0
shadowX1337z <ble396414@gmail.com>
2025-10-23 12:31:19 +0700

    Update tkinter_4.py

--- tkinter_4.py
+++ tkinter_4.py
@@ -1,6 +1,7 @@
 import tkinter,time
 from tkinter import *
 from tkinter import messagebox
+from tkinter import font
 
 root = Tk()
 root.geometry("500x400")
@@ -12,23 +13,27 @@ nen_desktop_dangki = Canvas(root,width = 400,height = 300,bg="#ADFFF5",highlight
 nen_desktop_dangki.place(x=50,y=30)
 
 
-thong_loi_thieu_thong_tin_name = Label(root, text="", fg="#ff0000", bg="#00ffd5")
+thong_loi_thieu_thong_tin_name = Label(root, text="", fg="#ff0000", bg="#ADFFF5")
 thong_loi_thieu_thong_tin_name.place(x=58,y=130)
 
-thong_loi_thieu_thong_tin_password = Label(root, text="", fg="#ff0000", bg="#00ffd5")
+thong_loi_thieu_thong_tin_password = Label(root, text="", fg="#ff0000", bg="#ADFFF5")
 thong_loi_thieu_thong_tin_password.place(x=58,y=230)
 
 
 # === HIỆU ỨNG NÚT ===
-def trong_nut(event):
+def trong_nut_dang_ki(event):
     dang_ky_button.config(bg="#32d424")
-def ra_ngoai_nut(event):
+def trong_nut_dang_nhap(event):
+    dang_nhap_button.config(bg="#32d424")
+def ra_ngoai_nut_dang_ki(event):
         dang_ky_button.config(bg="#00ff22")
+def ra_ngoai_nut_dang_nhap(event):
+        dang_nhap_button.config(bg="#00ff22")
 
 # === CÁC THÀNH PHẦN GIAO DIỆN ===
-dang_ki_head = Label(root,text="ĐĂNG KÍ",fg="#2200ff",bg="#00ffd5",font=("Arial",20))
-dang_ky_ten_head = Label(root,text="Nhập tên tại đây",fg="#00407d")
-dang_ky_password_head = Label(root,text="Nhập mật khẩu tại đây",fg="#00407d")
+dang_ki_head = Label(root,text="ĐĂNG KÍ",fg="#2200ff",bg="#ADFFF5",font=("Arial",20))
+dang_ky_ten_head = Label(root,text="Nhập tên tại đây",fg="#00407d",bg="#ADFFF5")
+dang_ky_password_head = Label(root,text="Nhập mật khẩu tại đây",fg="#00407d",bg="#ADFFF5")
 dang_ky_ten = Entry(root,width=35,font=("Arial",14))
 dang_ky_password = Entry(root,width=35,font=("Arial",14),show = "*")
 
@@ -77,12 +82,31 @@ def bam_nut_dang_ki():
         thong_loi_thieu_thong_tin_name.config(text="")
         thong_loi_thieu_thong_tin_password.config(text="")
 
+def bam_nut_dang_nhap():
+     root.title("ĐĂNG NHẬP")
+     dang_ki_head.config(text="ĐĂNG NHẬP",fg="#2200ff",bg="#9dffad",font=("Arial",20))
+     root.config(bg="#E2FF6D")
+     nen_desktop_dangki.config(bg="#9dffad")
+     dang_ky_ten_head.config(bg="#9dffad")
+     dang_ky_ten.config(bg="#9dffad")
+     dang_ky_password_head.config(bg="#9dffad")
+     dang_ky_password.config(bg="#9dffad")
+     thong_loi_thieu_thong_tin_name.config(bg="#9dffad")
+     thong_loi_thieu_thong_tin_password.config(bg="#9dffad")
+     dang_ky_button.place_forget()
+     thong_loi_thieu_thong_tin_name.config(text="")
+     thong_loi_thieu_thong_tin_password.config(text="")
+     
+
 def luu_info_vao_file(name, password):
     with open("data_info_user.txt", "a", encoding="utf-8") as file:
         file.write(f"{name}:{password}\n")
 
 #===tạo nút đăng kí===
 dang_ky_button = Button(root,text="ĐĂNG KÍ",fg="#b300ff",bg="#00ff22",font=("Arial",15),command=bam_nut_dang_ki)
+dang_nhap_button = Button(root,text="ĐĂNG NHẬP",fg="#b300ff",bg="#00ff22",font=("Arial",15))
+font_underline_ = font.Font(family="Arial", size=12, underline=1)
+goi_dang_nhap_button = Button(root,text="ĐĂNG_NHẬP",fg="#0034C5",bg="#00ffd5",font=font_underline_,bd=0,highlightthickness=0, relief="flat",command = bam_nut_dang_nhap)
 
 # === HÀM GHI LOG ===
 def click_vao_nhap_password(event):
@@ -111,7 +135,8 @@ dang_ky_ten.place(x=57,y=100)
 dang_ky_password_head.place(x=58,y=180)
 dang_ky_password.place(x=58,y=200)
 dang_ky_button.place(x=190,y=270)
-
+goi_dang_nhap_button.place(x=375,y=350)
+dang_nhap_button.place()
 #===gộp hàm===
 def log_nhap_ten(event):
      nhap_vao_nhap_name(event)
@@ -121,8 +146,10 @@ def log_nhap_password(event):
      nhan_nut_trong_ban_phim(event)
 
 # === GẮN SỰ KIỆN ===
-dang_ky_button.bind("<Enter>",trong_nut)
-dang_ky_button.bind("<Leave>",ra_ngoai_nut)
+dang_ky_button.bind("<Enter>",trong_nut_dang_ki)
+dang_ky_button.bind("<Leave>",ra_ngoai_nut_dang_ki)
+dang_nhap_button.bind("<Leave>",ra_ngoai_nut_dang_nhap)
+dang_nhap_button.bind("<Enter>",trong_nut_dang_nhap)
 dang_ky_ten.bind("<FocusIn>",click_vao_nhap_name)
 dang_ky_ten.bind("<Key>",log_nhap_ten)
 dang_ky_password.bind("<FocusIn>",click_vao_nhap_password)
